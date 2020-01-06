4fc634e70ab047abdc47a0d26f1b4bbbd885c6ca
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2020-01-06 17:19:04 +0800

    Update config.py

--- list_projects_py/config.py
+++ list_projects_py/config.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
 
-TOKEN="metrics_list"
+PROJECTS_TOPIC = "projects/YOUR_PROJECT_ID/topics/metrics_export_start"
+TOKEN = "metrics_list"
+ACTIVE = "ACTIVE"
+METRIC_EXPORT_PUBSUB_VERIFICATION_TOKEN = "16b2ecfb-7734-48b9-817d-4ac8bd623c87"
